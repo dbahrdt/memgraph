ae9b09e5d7cf1364b531daa4d270ee1159156072
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-07-19 15:00:42 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Graph.h"
+
+void help() {
+	std::cout << "memgraph file.osm.pbf" << std::endl;
+}
+
+int main(int argc, char ** argv) {
+	if (argc != 2) {
+		help();
+		return -1;
+	}	
+	std::string inFileName( argv[1] );
+	memgraph::Graph g( memgraph::Graph::fromPBF(inFileName) );
+	std::cout << "Nodes: " << g.nodeCount() << std::endl;
+	std::cout << "Edges: " << g.edgeCount() << std::endl;
+	return 0;
+}
