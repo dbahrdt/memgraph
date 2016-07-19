5993bdad4b956d5dce322b55a34949b15bdb19ef
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-07-19 15:08:47 +0200

    remove unused functions

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -12,17 +12,6 @@ inline double sqr(double a) { return a*a;}
 inline double toRadian(double deg) { return (deg*M_PI)/180;}
 inline double toDegree(double radian) { return (radian*180)/M_PI; }
 
-inline double bearingTo(double lat0, double lon0, double lat1, double lon1) {
-	double ph1(toRadian(lat0)), ph2(toRadian(lat1));
-	double deltaLambda = toRadian(lon1-lon0);
-	
-	double y = ::sin(deltaLambda) * ::cos(ph2);
-	double x = ::cos(ph1)* ::sin(ph2) - ::sin(ph1) * ::cos(ph2)* ::cos(deltaLambda);
-	double theta = ::atan2(y, x);
-
-	return ::fmod(toDegree(theta)+360.0, 360.0);
-}
-
 inline double distanceTo(double lat0, double lon0, double lat1, double lon1, double earthRadius = 6371000.0) {
 	double ph1 = toRadian(lat0);
 	double ph2 = toRadian(lat1);
@@ -34,18 +23,6 @@ inline double distanceTo(double lat0, double lon0, double lat1, double lon1, dou
 
 	return earthRadius * c;
 }
-
-inline double crossTrackDistance(double lat0, double lon0, double lat1, double lon1, double latq, double lonq) {
-	double radius(6371e3);
-
-	double delta13 = distanceTo(lat0, lon0, latq, lonq, radius)/radius;
-	double theta13 = toRadian( bearingTo(lat0, lon0, latq, lonq) );
-	double theta12 = toRadian( bearingTo(lat0, lon0, lat1, lon1) );
-
-	double dxt = ::asin( ::sin(delta13) * ::sin(theta13-theta12) ) * radius;
-	return dxt;
-}
-
 
 namespace memgraph {
 
